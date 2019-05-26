88fdd24aff600afc4bbb16dd68257d77334f463e
willbasky <vladislav.sabanov@gmail.com>
2019-05-26 13:15:18 +0300

    Create the project

---
+++ app/Main.hs
@@ -0,0 +1,7 @@
+module Main (main) where
+
+import MediaTransparencyAustria (someFunc)
+
+
+main :: IO ()
+main = someFunc
---
+++ src/MediaTransparencyAustria.hs
@@ -0,0 +1,6 @@
+module MediaTransparencyAustria
+       ( someFunc
+       ) where
+
+someFunc :: IO ()
+someFunc = putStrLn ("someFunc" :: String)
