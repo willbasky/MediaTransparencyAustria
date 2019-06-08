ddd9132c7896cc5e4e85d98c4be663d53540ccd1
willbasky <vladislav.sabanov@gmail.com>
2019-06-08 21:44:15 +0200

    Refactor

--- app/Main.hs
+++ app/Main.hs
@@ -8,35 +8,66 @@ module Main (main) where
 import Control.Concurrent.Async (mapConcurrently)
 import Control.Concurrent.MVar
 import Control.Monad (forever, void)
-import Text.Printf (printf)
+import Control.Monad.Except (ExceptT, runExceptT, throwError)
 import Data.Aeson
 import Data.Aeson.Types (parseEither)
 import Data.Either (rights)
-import Data.List (groupBy, nub, sortBy, nubBy)
-import Text.Read (readEither,read)
+import Data.List (groupBy, nub, nubBy, sortBy)
 import Data.Text (Text)
+import Data.Maybe (catMaybes)
+import Fmt
+import Fmt.Internal
 import GHC.Generics (Generic)
 import Network.HTTP.Conduit
 import System.Environment (getArgs)
 import System.Exit (exitSuccess)
 import System.IO (stdout)
-import Text.Pretty.Simple
-import Control.Monad.Except (ExceptT, throwError, runExceptT)
+import Text.Read (readEither)
 
 import qualified Data.ByteString.Char8 as BC
 import qualified Data.Text as T
+import qualified Data.Text.Encoding as E
 import qualified Data.Text.IO as T
-import Data.Text.Encoding as E
 
 
 -- Data type for the information from the JSON file
 data MediaInfo = MediaInfo
-    { rechtstraeger       :: Text
-    , quartal             :: Text
+    { rechtstraeger       :: Payer
+    , quartal             :: Quartal
     , bekanntgabe         :: Int
-    , mediumMedieninhaber :: Text
-    , euro                :: Float
+    , mediumMedieninhaber :: Recipient
+    , euro                :: Double
     } deriving (Show, Eq, Generic)
+
+type Payer = Text
+type Recipient = Text
+type Quartal = Text
+
+data Top = Top
+    { tPayer     :: Payer
+    , tRecipient :: Recipient
+    , tEuro      :: Double
+    } deriving (Show, Eq)
+
+emptyTop :: Top
+emptyTop = Top
+    { tPayer     = T.empty
+    , tRecipient = T.empty
+    , tEuro      = 0
+    }
+
+data Detail = Detail
+    { dAgent     :: Text
+    , dEuro      :: Double
+    , dCategory  :: Int
+    } deriving (Show, Eq)
+
+emptyDetail :: Detail
+emptyDetail = Detail
+    { dAgent    = T.empty
+    , dEuro      = 0
+    , dCategory  = 0
+    }
 
 -- Instances to convert our type to/from JSON.
 -- It is enough for simple types.
@@ -46,10 +77,10 @@ media :: Value -> Either String [MediaInfo]
 media = parseEither $ withObject "people" $ \o -> o .: "data"
 
 data QuartalOverview = QuartalOverview
-    { qoQuartal       :: Text
-    , qoBekanntgabe2  :: Float
-    , qoBekanntgabe4  :: Float
-    , qoBekanntgabe31 :: Float
+    { qoQuartal       :: Quartal
+    , qoBekanntgabe2  :: Double
+    , qoBekanntgabe4  :: Double
+    , qoBekanntgabe31 :: Double
     } deriving (Show, Eq)
 
 emptyOverView :: QuartalOverview
@@ -68,7 +99,7 @@ cli mi = forever $ do
     let command = words $ T.unpack query
     case command of
         (q:qs) -> case q of
-            "help" -> T.putStrLn helpText
+            "help" -> fmt helpText
             "exit" -> exitSuccess
             -- Set 2
             "payers" -> do
@@ -78,15 +109,14 @@ cli mi = forever $ do
                 ps <- readMVar mi
                 showRecipients ps
             -- Set 3
-            "quaters" -> do
+            "quarters" -> do
                 ps <- readMVar mi
-                printf "%s   %s    %s    %s\n"
-                    ("Quartal" :: String)
-                    ("Category 2" :: String)
-                    ("Category 4"  :: String)
-                    ("Category 31" :: String)
-                mapM_ (\QuartalOverview{..} -> printf "%s      %.2f   %.2f     %.2f\n"
-                    qoQuartal qoBekanntgabe2 qoBekanntgabe4 qoBekanntgabe31) $ overviewByQuartal ps
+                mapM_ (\QuartalOverview{..} -> fmtLn
+                    $ (build qoQuartal)
+                    <> (padLeftF 24 ' ' $ formatNum qoBekanntgabe2 <> "(§2),")
+                    <> (padLeftF 24 ' ' $ formatNum qoBekanntgabe4 <> "(§4),")
+                    <> (padLeftF 24 ' ' $ formatNum qoBekanntgabe31 <> "(§31)"))
+                    $ overviewByQuartal ps
             -- Set 4
             "load" -> do
                 let quartals = parseQuartalArgs qs
@@ -134,8 +164,11 @@ main = do
     readJSON $ parseQuartalArgs args
 
 -- Common functions
-toText :: Show a => a -> Text
-toText = T.pack . show
+sortByFieldDesc :: Ord a => (t -> a) -> [t] -> [t]
+sortByFieldDesc field = sortBy (\a b -> flip compare (field a) (field b))
+
+groupByFieldDesc :: Ord a => (t -> a) -> [t] -> [[t]]
+groupByFieldDesc field = groupBy (\a b -> field a == field b)
 
 unUmlaut :: Text -> Text
 unUmlaut txt = case T.uncons lowTxt of
@@ -152,12 +185,10 @@ unUmlaut txt = case T.uncons lowTxt of
 showPayers :: [MediaInfo] -> IO ()
 showPayers ps = do
     mapM_ T.putStrLn $ filterPayers ps
-    -- T.writeFile "payers.txt" $ T.unlines $ filterPayers ps
 
 showRecipients :: [MediaInfo] -> IO ()
 showRecipients ps = do
     mapM_ T.putStrLn $ filterRecipients ps
-    -- T.writeFile "recipients.txt" $ T.unlines $ filterRecipients ps
 
 filterPayers :: [MediaInfo] -> [Text]
 filterPayers = sortBy (\a b -> compare (unUmlaut a) (unUmlaut b)) . nub . map rechtstraeger
@@ -178,7 +209,17 @@ overviewByQuartal = map sumQuartal . groupBy (\a b -> quartal a == quartal b)
 
 -- Set 1 and Set 4
 parseQuartalArgs :: [String] -> [Int]
-parseQuartalArgs args = if null args then [lastQuartal] else map read args :: [Int]
+parseQuartalArgs [] = [lastQuartal]
+parseQuartalArgs args@(x:xs)
+    | null xs   = either (const [lastQuartal]) checkOne (readEither x :: Either String Int)
+    | otherwise = if null check then [lastQuartal] else check
+  where
+    rs = rights (map readEither (nub args) :: [Either String Int])
+    check = catMaybes $ map checkMany rs
+    checkNumber n = let (year, quart) = divMod n 10
+                    in and [1990 <= year, year <= 2100, 0 < quart, quart < 5]
+    checkMany n = if checkNumber n then Just n else Nothing
+    checkOne n = if checkNumber n then [n] else [lastQuartal]
 
 fetchAndParseJson :: [Int] -> IO [MediaInfo]
 fetchAndParseJson quartals = do
@@ -192,13 +233,28 @@ fetchAndParseJson quartals = do
 showTop :: [MediaInfo] -> Int -> String -> Int -> IO ()
 showTop mi n who cat = case who of
     "payers" -> mapM_
-        (\MediaInfo{..} -> T.putStrLn $ T.concat [rechtstraeger, "   ", toText euro]) sorted
+        (\Top{..} -> fmtLn $ padRightF 65 ' ' (build tPayer) <> padBothF 1 ' ' (":" :: Text) <> padLeftF 15 ' '  (formatNum tEuro))
+        (sortByEuro $ reduceAgents $ groupAgents rechtstraeger)
     "recipients" -> mapM_
-        (\MediaInfo{..} -> T.putStrLn $ T.concat [mediumMedieninhaber, "   ", toText euro]) sorted
+        (\Top{..} -> fmtLn $ padRightF 65 ' ' (build tRecipient) <> padBothF 1 ' ' (":" :: Text) <> padLeftF 15 ' '  (formatNum tEuro))
+        (sortByEuro $ reduceAgents $ groupAgents mediumMedieninhaber)
     _ -> pure ()
   where
-    sorted = take n $ sortBy (\a b -> flip compare (euro a) (euro b)) filterByCat
+    sortByEuro :: [Top] -> [Top]
+    sortByEuro = take n . sortByFieldDesc tEuro
+    reduceAgents :: [[MediaInfo]] -> [Top]
+    reduceAgents = map (foldr sumTopEuro emptyTop)
+    groupAgents :: (MediaInfo -> Payer) -> [[MediaInfo]]
+    groupAgents agent = groupByFieldDesc agent $ sortByFieldDesc agent filterByCat
+    filterByCat :: [MediaInfo]
     filterByCat = filter (\MediaInfo{..} -> cat == bekanntgabe) mi
+
+sumTopEuro :: MediaInfo -> Top -> Top
+sumTopEuro MediaInfo{..} Top{..} = Top
+    { tPayer = rechtstraeger
+    , tRecipient = mediumMedieninhaber
+    , tEuro = tEuro + euro
+    }
 
 parseTopArgs :: [String] -> ExceptT String IO (Int, String, Int)
 parseTopArgs [] = throwError "No arguments inputed."
@@ -220,16 +276,16 @@ readNumber = either (const $ Left "Wrong first argument. Number is awaited.") Ri
 
 readWho :: String -> Either String String
 readWho str = case str of
-    "payers" -> Right "payers"
+    "payers"     -> Right "payers"
     "recipients" -> Right "recipients"
-    _ -> Left "Wrong second argument. 'payers' or 'recipients' are awaited."
+    _            -> Left "Wrong second argument. 'payers' or 'recipients' are awaited."
 
 readCategory :: String -> Either String Int
 readCategory cat = case cat of
-    "2"  -> Right 2
-    "4"  -> Right 4
-    "31" -> Right 31
-    _  -> Left "Wrong category. 2,4,31 are awaited."
+    "§2"  -> Right 2
+    "§4"  -> Right 4
+    "§31" -> Right 31
+    _    -> Left "Wrong category. 2,4,31 are awaited."
 
 -- Set 6
 search :: Text -> Text -> [MediaInfo] -> IO ()
@@ -255,29 +311,47 @@ parse2StringArgs (str:query) = do
     pure (who, unwords query)
 
 -- Set 7
-details :: Text -> Text -> [MediaInfo] -> [MediaInfo]
-details who name ps = sortEuro found
+details :: Text -> Text -> [MediaInfo] -> [Detail]
+details who name ps = sortByFieldDesc dEuro foundByName
   where
-    found = case who of
-        "payers" -> filter (\MediaInfo{..} -> T.toLower name == T.toLower rechtstraeger) ps
-        "recipients" -> filter (\MediaInfo{..} -> T.toLower name == T.toLower mediumMedieninhaber) ps
+    foundByName = case who of
+        "payers" -> listAgents rechtstraeger mediumMedieninhaber
+        "recipients" -> listAgents mediumMedieninhaber rechtstraeger
         _ -> error "Wrong first argument. 'payers' or 'recipients' are awaited."
-    sortEuro = sortBy (\a b -> flip compare (euro a) (euro b))
 
-showDetails :: [MediaInfo] -> IO ()
+    listAgents :: (MediaInfo -> Text) -> (MediaInfo -> Text) -> [Detail]
+    listAgents who' name' = concatMap (reduceAgents who') $ map (groupAgents who') $ groupAgents bekanntgabe $ filterByAgent name' name
+    reduceAgents :: (MediaInfo -> Text) -> [[MediaInfo]] -> [Detail]
+    reduceAgents agent = map (foldr (sumDetailEuro agent) emptyDetail)
+    groupAgents agent = groupByFieldDesc agent . sortByFieldDesc agent
+
+    filterByAgent :: (MediaInfo -> Text) -> Text -> [MediaInfo]
+    filterByAgent agent name' = filter (\mi -> T.toLower name' == T.toLower (agent mi)) ps
+
+sumDetailEuro :: (MediaInfo -> Text) -> MediaInfo -> Detail -> Detail
+sumDetailEuro agent mi@MediaInfo{..} Detail{..} = Detail
+    { dAgent = agent mi
+    , dEuro = dEuro + euro
+    , dCategory = bekanntgabe
+    }
+
+showDetails :: [Detail] -> IO ()
 showDetails mi = do
-    let cat2 = filter (\MediaInfo{..} -> 2 == bekanntgabe) mi
-    let cat4 = filter (\MediaInfo{..} -> 4 == bekanntgabe) mi
-    let cat31 = filter (\MediaInfo{..} -> 31 == bekanntgabe) mi
+    writeFile "top.txt" $ show mi
+    let cat2 = filter (\Detail{..} -> 2 == dCategory) mi
+    let cat4 = filter (\Detail{..} -> 4 == dCategory) mi
+    let cat31 = filter (\Detail{..} -> 31 == dCategory) mi
     putStrLn "Payments according to §2:"
-    mapM_ (\MediaInfo{..} ->
-        T.putStrLn $ T.intercalate ", " [mediumMedieninhaber, T.pack $ show euro, "euro"]) cat2
+    pprintDetails cat2
     putStrLn "Payments according to §4:"
-    mapM_ (\MediaInfo{..} ->
-        T.putStrLn $ T.intercalate ", " [mediumMedieninhaber, T.pack $ show euro, "euro"]) cat4
+    pprintDetails cat4
     putStrLn "Payments according to §31:"
-    mapM_ (\MediaInfo{..} ->
-        T.putStrLn $ T.intercalate ", " [mediumMedieninhaber, T.pack $ show euro, "euro"]) cat31
+    pprintDetails cat31
+
+pprintDetails :: [Detail] -> IO ()
+pprintDetails ds =
+    mapM_ (\Detail{..} ->
+        fmtLn $ padRightF 70 ' ' (build dAgent) <> padLeftF 15 ' ' (formatNum dEuro)) ds
 
 -- Other
 jsonURL :: Int -> String
@@ -287,18 +361,34 @@ jsonURL yearQuart = concat
     , "&leermeldung=0&size=0"
     ]
 
-helpText :: Text
-helpText = T.unlines
-    [ "help                                          print this message"
-    , "exit                                          terminate this application"
-    , "payers                                        print a list of all payers"
-    , "recipients                                    print a list of all recipient"
-    , "quaters                                       print total sum for every payment category"
-    , "top n 'payers'|'recipients' '2'|'4'|'31'      print the n biggest payers|recipients for the given payment type"
-    , "search 'payers'|'recipients' searchTerm       print a list of all payers|recipients containing the given searchTerm"
-    , "load quarter1 quarter2 .. quarterN            load data for the given list of quarters"
-    , "details 'payers'|'recipients' organization    print a list of all payments payed or received by the given payers/recipient"
+helpText :: Builder
+helpText = unlinesF
+    [ "'help'" <> dots <> "print this message"
+    , "'exit'" <> dots <> "terminate this application"
+    , "'payers'" <> dots <> "print a list of all payers"
+    , "'recipients'" <> dots <> "print a list of all recipient"
+    , "'quarters'" <> dots <> "print total sum for every payment category"
+    , "'top' n 'payers'|'recipients' '§2'|'§4'|'§31'" <> dots <> "print the n biggest payers|recipients for the given payment type"
+    , "'search' 'payers'|'recipients' searchTerm" <> dots <> "print a list of all payers|recipients containing the given searchTerm"
+    , "'load' quarter1 quarter2 .. quarterN" <> dots <> "load data for the given list of quarters"
+    , "'details' 'payers'|'recipients' organization" <> dots <> "print a list of all payments payed or received by the given payers/recipient"
     ]
+  where
+    dots = padBothF 5 ' ' ("..." :: Text)
 
 lastQuartal :: Int
 lastQuartal = 20184
+
+-- Formating functions
+
+-- Get integer part of number and add dots between thousands
+formatInteger :: Double -> Builder
+formatInteger d = groupInt 3 '.' (floor d :: Integer)
+
+-- Get 2 digits of decimal part of number
+formatDecimal :: Double -> Builder
+formatDecimal = suffixF 2 . fixedF 2
+
+-- Build formated number. Aka "i.iii.iii,dd", "1.000,34"
+formatNum :: Double -> Builder
+formatNum n = formatInteger n <> "," <> formatDecimal n
